36dde1722a0fa6efe1f9324940098e56b7fad2f0
Harri <134476428+HarriKrishnaa@users.noreply.github.com>
2026-03-07 15:58:08 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+  },
+  images: {
+    unoptimized: true
+  },
+  output: 'standalone',
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate'
+          }
+        ]
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
